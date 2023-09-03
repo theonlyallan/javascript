7fda5973b87959043c90196f960f9c89a1bc375d
Allan Lombarde Ramos <allanlombardeallan@gmail.com>
2023-09-03 14:29:23 -0300

    Create ex012.js

---
+++ aulas/aula012/ex012.js
@@ -0,0 +1,11 @@
+var hora = 0
+console.log(`Agora são extamente ${hora} horas.`)
+if (hora >= 6) {
+    console.log('Bom dia !')
+} else if (hora <= 13) {
+    console.log(`Boa tarde !`)
+} else {
+    console.log(`Boa noite !`)
+} if (hora >= 0) {
+    console.log('Boa madrugada !')
+} 
