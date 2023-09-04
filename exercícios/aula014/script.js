6dad6f69da8db811c8a5b264a456e37e380353e6
Allan Lombarde Ramos <allanlombardeallan@gmail.com>
2023-09-04 16:29:05 -0300

    update

--- exercícios/aula014/script.js
+++ exercícios/aula014/script.js
@@ -1,23 +1,42 @@
-function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
+function verificar() {
     var data = new Date()
-    // var hora = data.getHours()
-    var hora = 12
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12) {
-        img.src = "fotomanha.png.png"
-        document.body.style.background = '#EDC79B'
-        msg.innerHTML = `Bom dia !`
-    } else if (hora >= 12 && hora < 18) {
-        img.src = "fototarde.png.png"
-        document.body.style.background = '#A6B1BB'
-        msg.innerHTML = `Boa Tarde !`
-
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        img.src = "fotonoite.png.png"
-        document.body.style.background = '#0E162F'
-        msg.innerHTML = `Bom Noite !`
-
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        //  res.innerHTML = `Idade calculada ${idade}`
+        var genero = ''
+        var img = document.createElement('img')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+              if  (idade >= 0 && idade < 10) {
+                //criança
+                  img.setAttribute('src,' 'foto-crianca-m')
+            } else if (idade < 21) {
+                //jovem
+            } else if (idade < 50) {
+                //adulto
+            } else {
+                //idoso
+            }
+        
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //criança
+            } else if (idade < 21) {
+                //jovem
+            } else if (idade < 50) {
+                //adulto
+            } else {
+                //idoso
+            }
+        }
+        res.style.textAling = "center"
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
     }
 }
