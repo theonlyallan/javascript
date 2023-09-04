baf81b7fde23f7b4aad715e7968e701c711c1229
Allan Lombarde Ramos <allanlombardeallan@gmail.com>
2023-09-04 00:49:34 -0300

    up

--- exercícios/aula013/script.js
+++ exercícios/aula013/script.js
@@ -1,14 +1,18 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('Imagem')
+    var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
+    // var hora = data.getHours()
+    var hora = 15
+    
     msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 8 && hora < 12) {
-img.scr = "noite01"
+    if (hora >= 0 && hora < 12) {
+    img.src = "fotomanha.png"
     } else if (hora >= 12 && hora < 18) {
+        img.src = "fototarde.png"
         //BOA TARDE !
     } else {
+        img.src = "fotonoite.png"
         // BOA NOITE
     }
 }
