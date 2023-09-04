f69598564248c604c5b5c73f97fea81da7b6ba7d
Allan Lombarde Ramos <allanlombardeallan@gmail.com>
2023-09-04 17:30:42 -0300

    finalizado

--- exercícios/aula014/script.js
+++ exercícios/aula014/script.js
@@ -15,28 +15,32 @@ function verificar() {
             genero = 'Homem'
               if  (idade >= 0 && idade < 10) {
                 //criança
-                  img.setAttribute('src', 'foto-crianca-m')
+                  img.setAttribute('src', 'foto-crianca-m.png')
             } else if (idade < 21) {
                 //jovem
-                  img.setAttribute('src', 'foto-jovem-m')
+                  img.setAttribute('src', 'foto-jovem-m.png')
             } else if (idade < 50) {
                 //adulto
-                  img.setAttribute('src', 'foto-homem-m')
+                  img.setAttribute('src', 'foto-homem-m.png')
             } else {
                 //idoso
-                  img.setAttribute('src', 'foto-idoso-m')
+                  img.setAttribute('src', 'foto-idoso-m.png')
             }
         
         } else if (fsex[1].checked) {
             genero = 'Mulher'
             if (idade >= 0 && idade < 10) {
                 //criança
+                img.setAttribute('src', 'foto-crianca-f.png')
             } else if (idade < 21) {
                 //jovem
+                img.setAttribute('src', 'foto-jovem-f.png')
             } else if (idade < 50) {
                 //adulto
+                img.setAttribute('src', 'foto-mulher-f.png')
             } else {
                 //idoso
+                img.setAttribute('src', 'foto-idosa-f.png')
             }
         }
         res.style.textAling = "center"
