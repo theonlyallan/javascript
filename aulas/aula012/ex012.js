4c61d20936a6b22c5e9c366fbcb2e6335acf9fb9
Allan Lombarde Ramos <allanlombardeallan@gmail.com>
2023-09-03 22:41:50 -0300

    ex013

--- aulas/aula012/ex012.js
+++ aulas/aula012/ex012.js
@@ -1,11 +1,10 @@
-var hora = 0
+var agora = new Date()
+var hora = agora.getHours()
 console.log(`Agora são extamente ${hora} horas.`)
-if (hora >= 6) {
+if (hora < 12) {
     console.log('Bom dia !')
-} else if (hora <= 13) {
+} else if (hora <= 18) {
     console.log(`Boa tarde !`)
 } else {
-    console.log(`Boa noite !`)
-} if (hora >= 0) {
-    console.log('Boa madrugada !')
-} 
+    console.log('Boa noite !')
+}
---
+++ aulas/aula012/ex013.js
@@ -0,0 +1,44 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+// 0 = domingo
+// 1 = segunda
+// 2 = terça
+// 3 = quarta 
+// 4 = quinta 
+// 5 = sexta
+// 6 = sabado
+switch (diaSem) {
+    case 0:
+        console.log('Domingo')
+        break
+}
+switch (diaSem) {
+    case 1:
+        console.log('Segunda')
+        break
+}
+switch (diaSem) {
+    case 2:
+        console.log('Terça')
+        break
+}
+switch (diaSem) {
+    case 3:
+        console.log('Quarta')
+        break
+}
+switch (diaSem) {
+    case 4:
+        console.log('Quinta')
+        break
+}
+switch (diaSem) {
+    case 5:
+        console.log('Sexta')
+        break
+}
+switch (diaSem) {
+    case 6:
+        console.log('Sabádo')
+        break
+}
