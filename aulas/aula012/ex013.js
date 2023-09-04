1323c6ad010576c47933b85c68be9afa1f1abd48
Allan Lombarde Ramos <allanlombardeallan@gmail.com>
2023-09-03 22:48:52 -0300

    Update ex013.js

--- aulas/aula012/ex013.js
+++ aulas/aula012/ex013.js
@@ -11,34 +11,38 @@ switch (diaSem) {
     case 0:
         console.log('Domingo')
         break
-}
-switch (diaSem) {
+
+
     case 1:
         console.log('Segunda')
         break
-}
-switch (diaSem) {
+
+
     case 2:
         console.log('Terça')
         break
-}
-switch (diaSem) {
+
+
     case 3:
         console.log('Quarta')
         break
-}
-switch (diaSem) {
+
+
     case 4:
         console.log('Quinta')
         break
-}
-switch (diaSem) {
+
+
     case 5:
         console.log('Sexta')
         break
-}
-switch (diaSem) {
+
+
     case 6:
         console.log('Sabádo')
+        break
+
+    default:
+        console.log('[ERRO] Dia inválido.')
         break
 }
