7010b7f3fff04eb247c8756c144b8e87c0c871ee
Allan Lombarde Ramos <allanlombardeallan@gmail.com>
2023-09-06 15:52:35 -0300

    Update ambiente.js

--- aulas/aula013/ambiente.js
+++ aulas/aula013/ambiente.js
@@ -1,16 +1,20 @@
 var c = 1
-while (c <= 4) {
+do {
     console.log(`Passo ${c}`)
-    c++ // c = c + 1
-}
+    c++
+} while (c <= 2)
+
+//MODO CERTO !!!
+// // var c = 1
+// while (c <= 4) {
+//     console.log(`Passo ${c}`)
+//     c++ // c = c + 1
+// }
 
 
 
 
-
-
-
-
+// MODO ERRADO !!!!
 // console.log('Tudo bem?')
 // console.log('Tudo bem?')
 // console.log('Tudo bem?')
