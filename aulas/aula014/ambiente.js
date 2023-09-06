1dbb505f21390fd3daedf60818c899f11dcabd95
Allan Lombarde Ramos <allanlombardeallan@gmail.com>
2023-09-06 16:26:39 -0300

    depuração

---
+++ aulas/aula014/ambiente.js
@@ -0,0 +1,12 @@
+// var c = 1
+// while (c <= 5) {
+//     console.log(c)
+//     c++
+// }
+
+//------------------------
+console.log('Vai começar')
+for (var c = 1; c <= 12; c++) {
+
+}
+console.log('FIM !')
