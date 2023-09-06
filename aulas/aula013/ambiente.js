25e9132e6b7901a1daf1d3907bb28517f893b8ed
Allan Lombarde Ramos <allanlombardeallan@gmail.com>
2023-09-06 15:48:42 -0300

    aula 13

---
+++ aulas/aula013/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+while (c <= 4) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+
+
+
+
+
+
+
+
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// onsole.log('Tudo bem?')
