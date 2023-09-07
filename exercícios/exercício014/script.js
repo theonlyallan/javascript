b6f0d487358336c3b90ca9c5308d08941f9daa00
Allan Lombarde Ramos <allanlombardeallan@gmail.com>
2023-09-07 16:17:40 -0300

    .

--- exercícios/exercício014/script.js
+++ exercícios/exercício014/script.js
@@ -1,50 +1,34 @@
-function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.querySelector('div#res')
-    if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('[ERRO] Verifique os dados e tente novamente!')
+function contar() {
+    let ini = document.getElementById('txt1');
+    let fim = document.getElementById('txtf');
+    let passo = document.getElementById('txtp');
+    let res = document.getElementById('res');
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!');
     } else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-        //  res.innerHTML = `Idade calculada ${idade}`
-        var genero = ''
-        var img = document.createElement('img')
-        if (fsex[0].checked) {
-            genero = 'Homem'
-              if  (idade >= 0 && idade < 10) {
-                //criança
-                  img.setAttribute('src', 'foto-crianca-m.png')
-            } else if (idade < 21) {
-                //jovem
-                  img.setAttribute('src', 'foto-jovem-m.png')
-            } else if (idade < 50) {
-                //adulto
-                  img.setAttribute('src', 'foto-homem-m.png')
-            } else {
-                //idoso
-                  img.setAttribute('src', 'foto-idoso-m.png')
+        res.innerHTML = 'Contando: ';
+        let i = Number(ini.value);
+        let f = Number(fim.value);
+        let p = Number(passo.value);
+
+        if (p <= 0) {
+            window.alert('Passo inválido. Será considerado passo 1.');
+            p = 1;
+        }
+
+        if (i < f) {
+            // Counting up
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449} `;
             }
-        
-        } else if (fsex[1].checked) {
-            genero = 'Mulher'
-            if (idade >= 0 && idade < 10) {
-                //criança
-                img.setAttribute('src', 'foto-crianca-f.png')
-            } else if (idade < 21) {
-                //jovem
-                img.setAttribute('src', 'foto-jovem-f.png')
-            } else if (idade < 50) {
-                //adulto
-                img.setAttribute('src', 'foto-mulher-f.png')
-            } else {
-                //idoso
-                img.setAttribute('src', 'foto-idosa-f.png')
+        } else {
+            // Counting down
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449} `;
             }
         }
-        res.style.textAling = "center"
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-        res.appendChild(img)
+
+        res.innerHTML += `\u{1F3C1}`;
     }
 }
