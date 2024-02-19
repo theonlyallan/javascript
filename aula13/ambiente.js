9def25a18d61b7cb6e95c0cc9b36be8dc44b2e77
Allan Lombarde Ramos <allanlombardeallan@gmail.com>
2024-02-19 00:44:01 -0300

    while e do

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,8 +1,19 @@
+
 let c = 1
-while (c < 6) {
-    console.log('Tudo bem?')
-    c++
-}
+
+do {
+    console.log(`Passo ${c}`);
+    c++;
+} while (c <= 6);
+
+
+
+
+/*while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++   
+} 
+*/
 
 
 
