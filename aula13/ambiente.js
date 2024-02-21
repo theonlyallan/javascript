9442b3a1e59f6acd7013b30681c39ae00eb2ad89
Allan Lombarde Ramos <allanlombardeallan@gmail.com>
2024-02-21 01:08:01 -0300

    Update ambiente.js

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,10 +1,21 @@
 
+/*
 let c = 1
 
 do {
     console.log(`Passo ${c}`);
     c++;
-} while (c <= 6);
+} while (c <= 6); */
+
+
+console.log(`Vai começar...`)
+for (var calculado = 1; c <= 5; c++) {
+    console.log(calculado)
+}
+console.log('FIM!!')
+
+
+
 
 
 
