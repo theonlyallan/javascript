8bd46441c290541c098dbaf6328cb7f7ed850218
Allan Lombarde Ramos <allanlombardeallan@gmail.com>
2024-03-13 15:40:46 -0300

    Update script.js

--- site-com-js/site-com-js-main/script.js
+++ site-com-js/site-com-js-main/script.js
@@ -3,6 +3,7 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
+    
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
