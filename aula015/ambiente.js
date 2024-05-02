eee1cd4cb50a19143a1a77aef683f15f9ac2feb4
Allan Lombarde Ramos <allanlombardeallan@gmail.com>
2024-05-02 16:09:19 -0300

    Arrays

---
+++ aula015/ambiente.js
@@ -0,0 +1,13 @@
+let num = [5, 8, 2, 9, 3]
+console.log(num)
+console.log(`Nosso vetor é o ${num.length} posições.`);
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+let pos = num.indexOf(8)
+if (pos == -1) {
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
+
+
+
